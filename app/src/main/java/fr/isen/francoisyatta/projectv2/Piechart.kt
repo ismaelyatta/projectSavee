91578d2f024a96aea3c70b04010c9a9941ae7cee
yacine FOSSO <yacine-zorah.fosso-kenfack@isen.yncrea.fr>
2023-05-12 13:11:10 +0200

    update

---
+++ app/src/main/java/fr/isen/francoisyatta/projectv2/Piechart.kt
@@ -0,0 +1,12 @@
+package fr.isen.francoisyatta.projectv2
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
+
+public class Piechart {
+
+}
